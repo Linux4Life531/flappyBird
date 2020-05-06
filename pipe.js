80905b46042344c7e3ad02d45f1feb9b14ce6189
Linux4Life531 <61314680+Linux4Life531@users.noreply.github.com>
2020-05-06 15:42:55 +0100

    Add basic files!

---
+++ pipe.js
@@ -0,0 +1,42 @@
+function Pipe() {
+    this.top = random(80, height/2 - 40);
+    this.bottom = random(80, height/2 - 40);
+    this.x = width;
+    this.w = 20;
+    this.speed = 2;
+    
+    this.white = 255;
+    
+    this.highlight = false;
+    
+    this.hits = function(bird) {
+      if (bird.y < this.top || bird.y > height - this.bottom) {
+        if (bird.x > this.x && bird.x < this.x + this.w) {
+          this.highlight = true;
+          return true;
+        }
+        this.highlight = false;
+      } return false;
+    }
+    
+    this.draw = function() {
+        fill(this.white);
+        if (this.highlight) {
+            fill(255, 0, 0);
+        }
+        rect(this.x, 0, this.w, this.top, 10);
+        rect(this.x, height-this.bottom, this.w, this.bottom, 10);
+    }
+        
+    this.update = function() {
+            this.x -= this.speed;
+    }
+        
+    this.offscreen = function() {
+        if (this.x < -this.w) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
---
+++ sketch.js
@@ -0,0 +1,68 @@
+var bird;
+var pipes = [];
+var gameOver = false;
+var time = 0;
+var newTime = 0;
+
+function setup() {
+  createCanvas(400, 600);
+  bird = new Bird();
+  pipes.push(new Pipe());
+  setInterval(timeDown, 1000);
+}
+
+function draw() {
+  
+  background(0);
+  
+  for (var i = pipes.length-1; i >= 0; i--) {
+    pipes[i].draw();
+    pipes[i].update();
+    
+    if (pipes[i].hits(bird)) {
+      console.log("HIT");
+      bird.lives -= 1;
+    }
+    
+    if (pipes[i].offscreen()) {
+      pipes.splice(i, 1);
+    }
+  }
+
+  bird.update();
+  bird.show();
+
+  if (bird.lives == 0) {
+    gameOver = true;
+    textSize(50);
+    fill(255, 0, 0);
+    text('Game over!', 20, 70);
+  }
+  
+  if (gameOver) {
+    textSize(50);
+    fill(255, 0, 0);
+    text('Game over!', 20, 70);
+  }
+
+  if (frameCount % 100 == 0) {
+    pipes.push(new Pipe());
+  }
+
+  textSize(50);
+  text(newTime, width-60, 70);
+  
+}
+
+function keyPressed() {
+  if (key == ' ') {
+    bird.up();
+  }
+}
+
+function timeDown(){
+  time++;
+  if (gameOver==false) {
+    newTime = time;
+  }
+}
